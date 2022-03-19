60a538c7da7eaf335153a5b9b35a8ae94064ae00
haen <dlgodls1231@naver.com>
2022-03-19 21:27:35 +0900

    add kkma

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,6 +1,12 @@
 import org.jsoup.Jsoup;
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -21,7 +27,7 @@ public class Indexer {
         DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docbuilder = docfactory.newDocumentBuilder();
 
-        Document document = (Document) docbuilder.newDocument();
+        Document document = docbuilder.newDocument();
 
         Element docs = document.createElement("docs");
         document.appendChild(docs);
@@ -64,7 +70,52 @@ public class Indexer {
         return dir.listFiles();
     }
     public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException {
-        makeFile();
+        //makeFile();
+        String testString ="꼬꼬마형테소분석기를 테스트 하고 있어여. 테스트 결과를 볼게요";
+
+        try {
+            File file = new File("collection.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document document = db.parse(file);
+
+            NodeList nList = document.getElementsByTagName("doc");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String txtString =  eElement.getElementsByTagName("body").item(0).getTextContent();
+                    KeywordExtractor ke = new KeywordExtractor();
+                    KeywordList kl = ke.extractKeyword(txtString, true);
+                    String content = "";
+                    for(int i = 0; i<kl.size(); i++){
+                        Keyword kwrd = kl.get(i);
+                        content += kwrd.getString()+":"+kwrd.getCnt()+"#";
+                    }
+                    eElement.getElementsByTagName("body").item(0).setTextContent(content);
+                }
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new FileOutputStream(new File("./index.xml")));
+
+            transformer.transform(source, result);
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
 
 
     }
