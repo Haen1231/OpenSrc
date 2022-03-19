784d29ce8e83feb603d3db538f2e3148f370c99f
haen <dlgodls1231@naver.com>
2022-03-19 21:26:35 +0900

    initial commit

---
+++ src/Indexer.java
@@ -0,0 +1,71 @@
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Indexer {
+    public static void makeFile() throws ParserConfigurationException, TransformerException, IOException{
+        String path = "src/data";
+        File [] file = makeFileList(path);
+
+        DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docbuilder = docfactory.newDocumentBuilder();
+
+        Document document = (Document) docbuilder.newDocument();
+
+        Element docs = document.createElement("docs");
+        document.appendChild(docs);
+
+        for(int i =0; i<5; i++) {
+
+            org.jsoup.nodes.Document html = Jsoup.parse(file[i], "UTF-8");
+            String titleData = html.title();
+            String bodyData = html.body().text();
+
+            Element doc = document.createElement("doc");
+            docs.appendChild(doc);
+
+            doc.setAttribute("id", "" + i + "");
+
+            Element title = document.createElement("title");
+            title.appendChild(document.createTextNode(titleData));
+            doc.appendChild(title);
+
+            Element body = document.createElement("body");
+            body.appendChild(document.createTextNode(bodyData));
+            doc.appendChild(body);
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(new FileOutputStream(new File("./collection.xml")));
+
+        transformer.transform(source, result);
+    }
+    public static File[] makeFileList(String path){
+        File dir = new File(path);
+        return dir.listFiles();
+    }
+    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException {
+        makeFile();
+
+
+    }
+}
