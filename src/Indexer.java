65eefcbfdf715e20abd0fde75bb65b2168b9dd74
haen <dlgodls1231@naver.com>
2022-03-30 01:54:00 +0900

    4주차 과제 - add indexer

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,7 +1,3 @@
-import org.jsoup.Jsoup;
-import org.snu.ids.kkma.index.Keyword;
-import org.snu.ids.kkma.index.KeywordExtractor;
-import org.snu.ids.kkma.index.KeywordList;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -12,104 +8,80 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Indexer {
-    public static void makeFile() throws ParserConfigurationException, TransformerException, IOException{
-        String path = "src/data";
-        File [] file = makeFileList(path);
+    public static HashMap<String, String> result = new HashMap<>();
+    public static ArrayList<ArrayList<HashMap<String, String>>> dataList = new ArrayList<>(); //index 저장
+    public static HashMap<String,String> keyMap = new HashMap<>();
+    public static Set<String> keys;
 
-        DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docbuilder = docfactory.newDocumentBuilder();
+    public static void init() throws IOException{
+        FileOutputStream fileStream = new FileOutputStream("src/index.post");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileStream);
+        keywordMap();
 
-        Document document = docbuilder.newDocument();
-
-        Element docs = document.createElement("docs");
-        document.appendChild(docs);
-
-        for(int i =0; i<5; i++) {
-
-            org.jsoup.nodes.Document html = Jsoup.parse(file[i], "UTF-8");
-            String titleData = html.title();
-            String bodyData = html.body().text();
-
-            Element doc = document.createElement("doc");
-            docs.appendChild(doc);
-
-            doc.setAttribute("id", "" + i + "");
-
-            Element title = document.createElement("title");
-            title.appendChild(document.createTextNode(titleData));
-            doc.appendChild(title);
-
-            Element body = document.createElement("body");
-            body.appendChild(document.createTextNode(bodyData));
-            doc.appendChild(body);
-        }
-
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-
-        Transformer transformer = transformerFactory.newTransformer();
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-
-        DOMSource source = new DOMSource(document);
-        StreamResult result = new StreamResult(new FileOutputStream(new File("./collection.xml")));
-
-        transformer.transform(source, result);
+        objectOutputStream.writeObject(result);
+        objectOutputStream.close();
     }
-    public static File[] makeFileList(String path){
-        File dir = new File(path);
-        return dir.listFiles();
-    }
-    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException {
-        //makeFile();
-        String testString ="꼬꼬마형테소분석기를 테스트 하고 있어여. 테스트 결과를 볼게요";
 
+    public static void keywordMap(){
         try {
-            File file = new File("collection.xml");
+            File file = new File("index.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
+            DocumentBuilder db = null;
+            try {
+                db = dbf.newDocumentBuilder();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
 
             Document document = db.parse(file);
 
             NodeList nList = document.getElementsByTagName("doc");
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
+                HashMap<String,String> data = new HashMap<>();
+                ArrayList<HashMap<String, String>> indexdata = new ArrayList<>();
                 Node nNode = nList.item(temp);
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
                     String txtString =  eElement.getElementsByTagName("body").item(0).getTextContent();
-                    KeywordExtractor ke = new KeywordExtractor();
-                    KeywordList kl = ke.extractKeyword(txtString, true);
-                    String content = "";
-                    for(int i = 0; i<kl.size(); i++){
-                        Keyword kwrd = kl.get(i);
-                        content += kwrd.getString()+":"+kwrd.getCnt()+"#";
+                    String[] splitarr = txtString.split("#");
+                    for (int i = 0; i< splitarr.length; i++){
+                        String[] arr = splitarr[i].split(":");
+                        keyMap.put(arr[0], arr[1]); //키만 받아오기 => keyset 생성을 위해
+                        data.put(arr[0], arr[1]); //index 별 키 저장
+                        indexdata.add(data);
+                        keys = keyMap.keySet();
                     }
-                    eElement.getElementsByTagName("body").item(0).setTextContent(content);
+
                 }
+                dataList.add(indexdata);
+            }
+            int N = dataList.size(); // 총 문서개수
+            for(String key : keys){
+                int[] tf = new int[5]; // 키 별 가중치 저장
+                double[] weight = new double[5];
+                int dfx=0; //
+                for(int i=0; i< dataList.size(); i++){ //index마다 돌기
+                    if(dataList.get(i).get(0).containsKey(key)){
+                        tf[i] = Integer.parseInt(dataList.get(i).get(0).get(key));
+                        dfx++;
+                    }
+                }
+                //가중치 계산
+                String resulttxt = "";
+                for(int j=0; j<N; j++){
+                    weight[j] = Math.round( tf[j] * Math.log( N / dfx )* 100) / 100.0;
+                    resulttxt += j + " " + weight[j]+ " ";
+                }
+                result.put(key, resulttxt);
+
             }
 
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(new FileOutputStream(new File("./index.xml")));
-
-            transformer.transform(source, result);
         }
         catch(IOException e) {
             System.out.println(e);
@@ -117,6 +89,29 @@ public class Indexer {
             e.printStackTrace();
         }
 
+    }
+    public static void fileInput() throws IOException, ClassNotFoundException {
+        FileInputStream fileStream = new FileInputStream("src/index.post");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
+
+        Object object = objectInputStream.readObject();
+        objectInputStream.close();
+
+        HashMap hashMap = (HashMap)object;
+        Iterator<String> it = hashMap.keySet().iterator();
+
+        while (it.hasNext()){
+            String key = it.next();
+            String value = (String) hashMap.get(key);
+            System.out.println(key+"->"+value);
+        }
+    }
+    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, ClassNotFoundException {
+        //makeCollectionFile();
+        //makeIndexFile();
+
+        init();
+        fileInput();
 
     }
 }
---
+++ src/makeKeyword.java
@@ -0,0 +1,76 @@
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class makeKeyword {
+    public static void makeIndexXML() throws TransformerException {
+        String testString ="꼬꼬마형테소분석기를 테스트 하고 있어여. 테스트 결과를 볼게요";
+
+        try {
+            File file = new File("collection.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = null;
+            try {
+                db = dbf.newDocumentBuilder();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+
+            Document document = db.parse(file);
+
+            NodeList nList = document.getElementsByTagName("doc");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String txtString =  eElement.getElementsByTagName("body").item(0).getTextContent();
+                    KeywordExtractor ke = new KeywordExtractor();
+                    KeywordList kl = ke.extractKeyword(txtString, true);
+                    String content = "";
+                    for(int i = 0; i<kl.size(); i++){
+                        Keyword kwrd = kl.get(i);
+                        content += kwrd.getString()+":"+kwrd.getCnt()+"#";
+                    }
+                    eElement.getElementsByTagName("body").item(0).setTextContent(content);
+                }
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new FileOutputStream(new File("./index.xml")));
+
+            transformer.transform(source, result);
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+}
