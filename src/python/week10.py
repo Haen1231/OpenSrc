ccbcacb7d5f12ffec399c1a7ff1452b32db8b7fe
haen <dlgodls1231@naver.com>
2022-05-11 22:41:15 +0900

    add sub title

--- src/python/week10.py
+++ src/python/week10.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    print("실습 1")
     str="""   죽는 날까지 하늘을 우러러
     한 점 부끄럼이 없기를,
     잎새에 이는 바람에도
@@ -11,3 +12,50 @@ if __name__ == '__main__':
     str=str.strip()
     str=str.replace("나","너")
     print(str)
+    
+    print("--------------------------------------")
+    print("실습 2")
+    str=input("Numbers=?")
+    strlist=str.split()
+    
+    max=int(strlist[0])
+    
+    
+    print("Numbers=? ")
+    print(max)
+    for str1 in strlist[1:]:
+        num=int(str1)
+        print(num)
+        if(num>max):
+            max = num
+            
+    print("Max=", max)
+    
+    print("--------------------------------------")
+    print("실습 3")
+    def conv_asc(str, num):
+        new_str=""
+        for i in str:
+            code=ord(i)
+            new_str += chr(code+num)
+        return new_str
+    
+    def enc(str, key):
+        list = str.split()
+        list = list[::-1]
+        new_str=" ".join(list)
+        return conv_asc(new_str,key)
+    
+    def dec(str, key):
+        str=conv_asc(str,key*-1)
+        list = str.split()
+        list = list[::-1]
+        new_str=" ".join(list)
+        return new_str
+    
+    str = "abc def"
+    enc_str= enc(str,1)
+    print("\""+str+"\"is encoded into \""+enc_str+"\"")
+    
+    dec_str= dec(enc_str,1)
+    print("\""+enc_str+"\"is decoded into \""+dec_str+"\"")
